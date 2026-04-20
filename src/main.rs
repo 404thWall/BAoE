0009ca42c8ca9bbd224b371e44b9a2ecb134e7a9
Eloan Danesi <eloan83600@gmail.com>
2026-04-21 00:31:16 +0200

    Added simple fps counter.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use bevy::dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin};
+use bevy::prelude::*;
+use bevy::text::FontSmoothing;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            // The built-in FPS overlay
+            FpsOverlayPlugin {
+                config: FpsOverlayConfig {
+                    text_config: TextFont {
+                        font_size: 24.0, // Big enough to see
+                        font: default(),
+                        font_smoothing: FontSmoothing::default(),
+                        ..default()
+                    },
+                    text_color: Color::srgb(0.0, 1.0, 0.0), // Classic green
+                    enabled: true,
+                    ..default()
+                },
+            },
+        ))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    // You MUST have a camera spawned to see the UI overlay!
+    commands.spawn(Camera2d);
 }

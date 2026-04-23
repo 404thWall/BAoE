e0ef1582839a0037cd05c2c9be972f6ddc58ee2d
Eloan Danesi <eloan83600@gmail.com>
2026-04-23 14:25:31 +0200

    Removed bevy. Added base window using winit and soon wgpu.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,77 @@
-use bevy::dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin};
-use bevy::prelude::*;
-use bevy::text::FontSmoothing;
-use bevy::window::PresentMode;
+use std::sync::Arc;
 
-fn main() {
-    App::new()
-        .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Imperial Rust".into(),
-                // This is the V-Sync setting:
-                // Immediate = V-sync OFF (Maximum FPS, may cause screen tearing)
-                // Mailbox = "Fast" V-sync (High FPS, no tearing, low latency)
-                present_mode: PresentMode::Immediate, 
-                ..default()
-            }),
-            ..default()
-        }),
-            // The built-in FPS overlay
-            FpsOverlayPlugin {
-                config: FpsOverlayConfig {
-                    text_config: TextFont {
-                        font_size: 24.0, // Big enough to see
-                        font: default(),
-                        font_smoothing: FontSmoothing::default(),
-                        ..default()
-                    },
-                    text_color: Color::srgb(0.0, 1.0, 0.0), // Classic green
-                    enabled: true,
-                    ..default()
-                },
-            },
-        ), )
-        .add_systems(Startup, setup)
-        .run();
+use fast_time::Clock;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::raw_window_handle::{HasDisplayHandle, HasWindowHandle};
+use winit::window::{Window, WindowId};
+mod example;
+mod render;
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+    count: u32,
 }
 
-fn setup(mut commands: Commands) {
-    // You MUST have a camera spawned to see the UI overlay!
-    commands.spawn(Camera2d);
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in AboutToWait, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+
+                // Draw.
+
+                // Queue a RedrawRequested evenxt.
+                //
+                // You only need to call this if you've determined that you need to redraw in
+                // applications which do not always need to. Applications that redraw continuously
+                // can render here instead.
+                self.window.as_ref().unwrap().request_redraw();
+                self.count += 1;
+            }
+            _ => (),
+        }
+    }
+}
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+
+    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+    // dispatched any events. This is ideal for games and similar applications.
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    // ControlFlow::Wait pauses the event loop if no events are available to process.
+    // This is ideal for non-game applications that only update in response to user
+    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+    //event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    let mut clock = Clock::new();
+    let start = clock.now();
+    event_loop.run_app(&mut app).unwrap();
+    let elapsed = start.elapsed(&mut clock);
+    println!("App ran for {}s", elapsed.as_secs_f64());
+    println!(
+        "Average fps was : {}",
+        app.count as f64 / elapsed.as_secs_f64()
+    );
 }
